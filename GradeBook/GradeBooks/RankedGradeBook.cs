9e2309f1ce17b47ce851ce507599043912a231ce
sarimq <36866784+sarimq@users.noreply.github.com>
2018-02-27 13:36:52 -0600

    Create GetLetterGradeOverride on Ranked

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,6 +9,19 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) :base(name)
         {
             Type = Enums.GradeBookType.Ranked;
-        } 
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            char grade = new char();
+            var count = Students.Count;
+            if(count < 5)
+            {
+                throw new InvalidOperationException("You cannot grade with less than 5 students.");
+
+                
+            }
+            return grade;
+        }
     }
 }
