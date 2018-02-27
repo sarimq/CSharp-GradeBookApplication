644ed09788672f4ef7919b44c964a593b9baf1bb
sarimq <36866784+sarimq@users.noreply.github.com>
2018-02-27 14:08:37 -0600

    attempted to rank

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,14 +14,56 @@ namespace GradeBook.GradeBooks
         public override char GetLetterGrade(double averageGrade)
         {
             char grade = new char();
-            var count = Students.Count;
-            if(count < 5)
-            {
-                throw new InvalidOperationException("You cannot grade with less than 5 students.");
+            int position = new int();
+            List<double> averageGrades = new List<double>();
 
-                
+            var count = Students.Count;
+            var bracketsize = count / 5;
+            if (count < 5)
+            {
+                throw new InvalidOperationException("You cannot grade with less than 5 students.");        
             }
-            grade = 'F';  
+
+            foreach (var student in Students)
+            {
+                averageGrades.Add(student.AverageGrade);
+
+
+            }
+
+            averageGrades.Sort();
+
+            for (int i = 0; i <= averageGrades.Count; i++)
+            {
+                if (averageGrade < averageGrades[i])
+                {
+                    continue;
+                }
+                else
+                {
+                    position = i; 
+                }
+            }
+
+            if(position <= bracketsize)
+            {
+                grade =  'A';
+            }
+            if (position <= bracketsize*2)
+            {
+                grade = 'B';
+            }
+            if (position <= bracketsize*3)
+            {
+                grade = 'C';
+            }
+            if (position <= bracketsize*4)
+            {
+                grade = 'D';
+            }
+
+
+
             return grade;
         }
     }
